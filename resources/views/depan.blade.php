58a0f42a13f9f1466b5501c080c17c1270f67a99
MeilyBenedicta <meilybenedicta2001@gmail.com>
2021-05-30 21:02:34 +0700

    perbaikan

--- resources/views/depan.blade.php
+++ resources/views/depan.blade.php
@@ -69,6 +69,16 @@
                 <a class="p-2 link-secondary" href="#">Layanan</a>
                 <a class="p-2 link-secondary" href="#">Cek Peminjaman</a>
                 <a class="p-2 link-secondary" href="#">Hubungi</a>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </div>
             </nav>
         </div>
     </div>
